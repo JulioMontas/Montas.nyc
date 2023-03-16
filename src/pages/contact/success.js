bd055728c0a152671b8326eaf5fef9d4fd14d831
JulioMontas <julio86@gmail.com>
2023-03-15 22:33:42 -0400

    Confetti bug fix

--- src/pages/contact/success.js
+++ src/pages/contact/success.js
@@ -1,13 +1,34 @@
 import React from 'react';
 import Layout from "../../components/layout"
-import useWindowSize from "use-window-size-v2";
 import Confetti from 'react-confetti'
+
+
+const useWindowSize = () => {
+  const [windowSize, setWindowSize] = React.useState({
+    width: undefined,
+    height: undefined,
+  });
+
+  React.useEffect(() => {
+    const handleResize = () =>
+      setWindowSize({ width: window.innerWidth, height: window.innerHeight });
+
+    window.addEventListener('resize', handleResize);
+
+    handleResize();
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  return windowSize;
+};
+
 
 export default function SuccessPage() {
 
-  
   const { width, height } = useWindowSize(100); // wait 100ms for the resize
-  console.log(height);
   
   return (
     <Layout>
