e7d1e9ae2dc1caf08549aed89d68523306ba6fc8
JulioMontas <julio86@gmail.com>
2023-03-17 20:50:14 -0400

    install contentful

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,7 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   siteMetadata: {
     title: `Montas.NYC`,
@@ -30,6 +34,13 @@ module.exports = {
       options: {
         "trackingId": "00000000"
       }
+    },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
     },
     "gatsby-plugin-image",
     "gatsby-plugin-react-helmet",
